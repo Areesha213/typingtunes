ffa6e29b8d319c45744b3804326c32f35984178f
Areesha213 <aqrsh213@gmail.com>
2025-03-10 00:25:03 +0500

    make some changes on middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,17 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
     const res = NextResponse.next();
 
-    res.headers.set("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.typingtunes.com;");
+    res.headers.set(
+        "Content-Security-Policy",
+        `default-src 'self'; 
+        script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.typingtunes.com https://www.googletagmanager.com https://www.google-analytics.com; 
+        style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; 
+        font-src 'self' https://fonts.gstatic.com; 
+        img-src 'self' data: https://www.google-analytics.com; 
+        media-src 'self' https://files.freemusicarchive.org;
+        connect-src 'self' https://www.google-analytics.com;`
+    );
+
     res.headers.set("X-Frame-Options", "DENY");
     res.headers.set("X-Content-Type-Options", "nosniff");
     res.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
